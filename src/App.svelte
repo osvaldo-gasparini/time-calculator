2fc9332c7b47a1102544d1badd4918422e2e98a7
osvaldo-gasparini <109749756+osvaldo-gasparini@users.noreply.github.com>
2025-09-13 17:37:29 -0300

    start project

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import TimeCalculator from './lib/TimeCalculator.svelte';
+</script>
+
+<main>
+  <p>placeholder</p>
+</main>
+
+<style>
+  main {
+    min-height: 100vh;
+    background: linear-gradient(135deg, #f8fafc 0%, #e2e8f0 100%);
+  }
+</style>
